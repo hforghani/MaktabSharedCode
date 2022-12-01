0c95cbfbf6fd05cadb31349514ec58cd160a1aa4
hossein <hossein.forghani@gmail.com>
2022-12-01 11:54:51 +0330

    Script added to myadd. LearnPython samples added.

--- celery_samples/myadd.py
+++ celery_samples/myadd.py
@@ -1,14 +1,28 @@
 import time
+from celery import Celery
+
+app = Celery('myadd', backend='redis://localhost', broker='redis://localhost')
 
 
-def add(x, y):
-    return x + y
-
-
-if __name__ == '__main__':
+@app.task
+def add():
     t = time.time()
-    for i in range(10 ** 8):
-        add(9999, 9999)
-        if i % 10 ** 7 == 0:
-            print(i)
+    for _ in range(10 ** 9):
+        x = 9999 + 9999  # However it is a useless code!!!
     print(f'took {time.time() - t} s')
+    return x
+
+
+results = []
+count = 10
+for _ in range(count):
+    res = add.delay()
+    results.append(res)
+
+ready = set()
+while len(ready) < count:
+    time.sleep(3)
+    for res in results:
+        if res not in ready and res.ready():
+            print(res.get())
+            ready.add(res)
